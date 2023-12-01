1034de2164e455f743da2df9168c3e957a1bcea8
SelMel-Venkatesh <jrvenk@gmail.com>
2023-12-01 10:22:43 +0530

    add comment line

--- PersonalTasks/Day01/03_variable_const.js
+++ PersonalTasks/Day01/03_variable_const.js
@@ -3,9 +3,9 @@
 // const cannot be declared without assigning any value.
 // /Reinitialization of const not allowed
 
-const day=7;
+const day = 7;
 day = 10;
 //TypeError: Assignment to constant variable.
 console.log(day);
 // const' declarations must be initialized.
-const p; 
+//const p;
